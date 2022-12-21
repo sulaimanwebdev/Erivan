943b7d18189419c25d84595381d1d8ab3e4b5d1d
Muhammad Sulaiman <trendingofyt@gmail.com>
2022-12-21 23:39:48 +0500

    errors fixed

--- pages/careers/index.js
+++ pages/careers/index.js
@@ -1,8 +1,8 @@
 import Link from 'next/link'
 import Head from 'next/head'
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import ScrollToTop from '../components/ScrollToTop';
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import ScrollToTop from '../../components/ScrollToTop';
 
 export default function Careers() {
 let team = [
